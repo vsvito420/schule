997f21cc5ac64b704fa39207ec98aee5cf28f05e
VitoS <ikn0@proton.me>
2024-01-11 10:18:10 +0100

    aufgabe hinzugefügt

---
+++ Funke/Schiff Struct/version1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/*  - Dieser Code ist eine schulaufgabe zur übung von structs.
+    - Die aufgabe besteht darin ein Schiff zu recyclen und alle daten in ein struct zu speichern-
+    - Das schiff wird in bereiche unterteilt: Rumpf, Motor, Treibstofftank , Nutzfläche, Innenaustattung, Passagierraum
+*/
+
+class Schiff {
+    string name;
+    float gesammtGewicht;
+    
+    // Materialien
+    float material_Aluminium;
+    float material_Anker;
+    float material_Holz;
+    float material_Kunststoff;
+    float material_Stahl;
+    float material_Sonstiges;
+    
+    //Aufbau
+    //Antrieb
+    //Innenausstattung
+    //Rumpf
+    //Nutzfläche
+}
+
+int main() {
+
+    return 0;
+}
