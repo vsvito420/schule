a1468b254ee4ba7120a615eeb2506e0166334f0d
VitoS <ikn0@proton.me>
2024-01-16 19:02:00 +0100

    added code :O

--- Funke/Schiff Struct/version1.cpp
+++ Funke/Schiff Struct/version1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 /*  - Dieser Code ist eine schulaufgabe zur übung von structs.
     - Die aufgabe besteht darin ein Schiff zu recyclen und alle daten in ein struct zu speichern-
@@ -12,26 +13,80 @@ using namespace std;
 */
 
 class Schiff {
-    public:
+    protected:
     string name;
     float gesammtGewicht;
     
     // Materialien
-    float material_Aluminium;
-    float material_Anker;
-    float material_Holz;
-    float material_Kunststoff;
-    float material_Stahl;
-    float material_Sonstiges;
+    float m_Aluminium;
+    float m_Anker;
+    float m_Holz;
+    float m_Kunststoff;
+    float m_Stahl;
+    float m_Sonstiges;
 };
 
 class Menu:: Schiff {
-    
+    private:
+    void antrieb() {
+        map <string, float> antrieb;
+        cout << "Welche materialien wurden verwendet? (mengenangabe in tonnen) \n";
+        cout << "Aluminium: ";
+        cin >> antrieb["Aluminium"];
+        cout << "Anker: ";
+        cin >> antrieb["Anker"];
+        cout << "Holz: ";
+        cin >> antrieb["Holz"];
+        cout << "Kunststoff: ";
+        cin >> antrieb["Kunststoff"];
+        cout << "Stahl: ";
+        cin >> antrieb["Stahl"];
+        cout << "Sonstiges: ";
+        cin >> antrieb["Sonstiges"];
+        cout << "Recycling informationen des Antriebes wurden erfolgreich eingetragen! \n";
+        eingabe();
+    }
+    public:
+    void eingabe() {
+        cout << "Bitte geben sie den Namen des Schiffes ein: ";
+        cin >> name;
+        cout << "Bitte geben sie das Gesammtgewicht des Schiffes ein: ";
+        cin >> gesammtGewicht;
+        cout << "Was möchten sie eingeben? \n";
+        cout << "1. Antrieb \n";
+        cout << "2. Innenausstattung \n";
+        cout << "3. Rumpf \n";
+        cout << "4. Nutzfläche \n";
+        cout << "5. Beenden \n";
+        int eingabe;
+        cin >> eingabe;
+        switch (eingabe) {
+            case 1:
+                antrieb();
+                break;
+            case 2:
+                innenausstattung();
+                break;
+            case 3:
+                rumpf();
+                break;
+            case 4:
+                nutzflaeche();
+                break;
+            case 5:
+                break;
+            default:
+                cout << "Falsche Eingabe! \n";
+                eingabe();
+                break;
+        }
+
+    }
 }
 
 int main() {
     Menu menu;
-
+    menu.eingabe();
 
     return 0;
 }
